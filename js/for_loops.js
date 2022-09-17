9b8d64c880d84e1f8d0982ab85ff932bac8dce3c
Josef Fussell <josef.k.fussell@gmail.com>
2022-09-17 13:19:30 -0500

    completed all exercises required to be in for_loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,68 @@
+//Create a function named showMultiplicationTable
+//Function accepts a number
+//Function console.logs the multiplication table for that number
+
+
+// function showMultiplicationTable(number) {
+//     for (let i = 0; i <= 10; i++) {
+//         let result = i * number;
+//             console.log(`${number} * ${i} = ${result}`);
+//         if (i === 10) {
+//             break;
+//         }
+//     }
+// }
+//
+// showMultiplicationTable(7);
+
+
+
+// Use a for loop to generate 10 random numbers between 20 and 200
+//output the result in console
+//console will output if the number is odd or even
+//create a function to determine if a number is odd or even
+
+// let randomNumber = Math.floor(Math.random() * 180) + 21 //add 20 to 180 to set range so range is 20-200
+//
+// for (let i = 0; i < 10; i++) { //creates a loop that loops 10 times
+//     let randomNumber = Math.floor(Math.random() * 180) + 21
+//         if (randomNumber % 2 === 0){
+//             console.log(`${randomNumber} is even`)
+//         } else {
+//             console.log(`${randomNumber} is odd`)
+//         }
+// }
+
+
+
+//extracurricular refactor with a function
+
+// for (let i = 0; i < 10; i++) { //creates a loop that loops 10 times
+//     let randomNumber = Math.floor(Math.random() * 180) + 21
+//     isEven(randomNumber);
+// }
+//
+//
+//
+// function isEven(input) {
+//     if (input % 2 === 0) {
+//         console.log(input + ' is even');
+//     } else {
+//         console.log(input  + ' is odd');
+//     }
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`${i}`.repeat(i));
+// }
+
+// for (let i = 100; i<= 100; i-=5) {
+//     console.log(`${i}`);
+//     if (i === 5) {
+//         break
+//     }
+// }
+
+
+
