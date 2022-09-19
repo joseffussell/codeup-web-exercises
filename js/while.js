b7ddf6b30517c4a9058d66732c1f3b76c510d9cd
Josef Fussell <josef.k.fussell@gmail.com>
2022-09-19 15:33:10 -0500

    started foundation for do while ice cream cones sales

--- js/while.js
+++ js/while.js
@@ -4,20 +4,33 @@ console.log('sanity check');
 // create a, while loop
 // console log to show doubled result
 
-let i = 0
+// let i = 0
+//
+// do {
+//     console.log(Math.pow(2, i++));
+//
+// } while (i <= 16);
+
+
+
+
+//write code that generates a random number between 50-100 ✅
+//above code will be used to represent the amount of cones to sell before
+//starting the loop ✅
+//inside of the loop code another rng for 1-5, this simulates the amount of
+//cones being bought by clients. ✅
+//use a do-while loop
+//log the amount of cones sold to each person
+
+let allCones = Number(Math.floor(Math.random() * 50) + 50);
+    console.log(allCones);
+
+let i = allCones
 
 do {
-    console.log(Math.pow(2, i++));
-
-} while (i <= 16);
-
-
-
-
-
-
-
-
+    let howManyCones = Number(Math.floor(Math.random() * 5) + 1);
+    console.log(allCones - (howManyCones + ' cones sold'))
+} while (i <= allCones);
 
 
 
