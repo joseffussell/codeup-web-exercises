b5812da1866c1395af5051ea2fd57118a58a5c3a
Josef Fussell <josef.k.fussell@gmail.com>
2022-09-19 15:11:42 -0500

    completed and tested while loops exercise

--- js/while.js
+++ js/while.js
@@ -1,1 +1,23 @@
 console.log('sanity check');
+
+
+// create a, while loop
+// console log to show doubled result
+
+let i = 0
+
+do {
+    console.log(Math.pow(2, i++));
+
+} while (i <= 16);
+
+
+
+
+
+
+
+
+
+
+
